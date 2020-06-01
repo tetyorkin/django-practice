6ca51eea3819b9bbba076d2f49fe30a69c89d22f
tetyorkin <tetyorkin@gmail.com>
2020-06-01 21:32:10 +0600

    Done task1

--- request-handling/landing/app/views.py
+++ request-handling/landing/app/views.py
@@ -1,34 +1,39 @@
 from collections import Counter
 
+from django.http import HttpResponse
 from django.shortcuts import render_to_response
 
-# Для отладки механизма ab-тестирования используйте эти счетчики
-# в качестве хранилища количества показов и количества переходов.
-# но помните, что в реальных проектах так не стоит делать
-# так как при перезапуске приложения они обнулятся
 counter_show = Counter()
 counter_click = Counter()
 
 
 def index(request):
-    # Реализуйте логику подсчета количества переходов с лендига по GET параметру from-landing
+    response = request.GET.get('from-landing')
+    if response == 'original':
+        counter_click['original'] += 1
+    elif response == 'test':
+        counter_click['test'] += 1
     return render_to_response('index.html')
 
 
 def landing(request):
-    # Реализуйте дополнительное отображение по шаблону app/landing_alternate.html
-    # в зависимости от GET параметра ab-test-arg
-    # который может принимать значения original и test
-    # Так же реализуйте логику подсчета количества показов
+    response = request.GET.get('ab-test-arg')
+    if response == 'test':
+        counter_show['test'] += 1
+        return render_to_response('landing_alternate.html')
+    elif response == 'original':
+        counter_show['original'] += 1
+        return render_to_response('landing.html')
     return render_to_response('landing.html')
 
 
 def stats(request):
-    # Реализуйте логику подсчета отношения количества переходов к количеству показов страницы
-    # Чтобы отличить с какой версии лендинга был переход
-    # проверяйте GET параметр marker который может принимать значения test и original
-    # Для вывода результат передайте в следующем формате:
+    if counter_show['original'] == 0 or counter_show['test'] == 0:
+        msg = 'Просмотров не было'
+        return HttpResponse(msg)
+    test_conversion = counter_click['test'] / counter_show['test']
+    original_conversion = counter_click['original'] / counter_show['original']
     return render_to_response('stats.html', context={
-        'test_conversion': 0.5,
-        'original_conversion': 0.4,
+        'test_conversion': test_conversion,
+        'original_conversion': original_conversion,
     })
