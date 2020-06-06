2db387c57e2616f298ea108a09286b40ef0994a3
tetyorkin <tetyorkin@gmail.com>
2020-06-06 21:48:34 +0600

    Done Task_1

--- dynamic-templates/task1/app/views.py
+++ dynamic-templates/task1/app/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render
+from app import settings
+
+
+def read_csv():
+    lines = []
+    with open(settings.INFLATION_RUSSIA, encoding='utf-8') as csv_file:
+        column = csv_file.readline().split(';')
+        for raw in csv_file:
+            cols = [float(x) if x else '' for x in raw.split(';')]
+            cols[0] = int(cols[0])
+            lines.append(cols)
+    return column, lines
+
 
 def inflation_view(request):
     template_name = 'inflation.html'
+    context = {
+        'column': read_csv()[0],
+        'lines': read_csv()[1],
+    }
+    return render(request, template_name, context)
 
-    # чтение csv-файла и заполнение контекста
-    context = {}
 
-    return render(request, template_name,
-                  context)
+if __name__ == '__main__':
+    pass
