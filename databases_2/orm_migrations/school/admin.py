7b8b3b7920384c2bcd6e601186236065c523d819
tetyorkin <tetyorkin@gmail.com>
2020-06-20 18:53:44 +0600

    orm_migrate done

--- databases_2/orm_migrations/school/admin.py
+++ databases_2/orm_migrations/school/admin.py
@@ -3,8 +3,14 @@ from django.contrib import admin
 from .models import Student, Teacher
 
 
+class TeachersForStudentsInline(admin.TabularInline):
+    model = Student.teacher.through
+    extra = 1
+
+
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
+    # inlines = [TeachersForStudentsInline]
     pass
 
 
--- databases_2/orm_migrations/school/views.py
+++ databases_2/orm_migrations/school/views.py
@@ -5,11 +5,9 @@ from .models import Student
 
 
 def students_list(request):
-    template = 'school/students_list.html'
-    context = {}
-
-    # используйте этот параметр для упорядочивания результатов
-    # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
     ordering = 'group'
-
+    template = 'school/students_list.html'
+    context = {
+        'object_list': Student.objects.all().prefetch_related('teacher').order_by(ordering)
+    }
     return render(request, template, context)
