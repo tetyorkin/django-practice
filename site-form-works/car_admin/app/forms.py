98ae13e7030a89e68d8e32ba117e8579788c12cc
tetyorkin <tetyorkin@gmail.com>
2020-06-20 20:18:27 +0600

    car_admin done

--- site-form-works/car_admin/app/forms.py
+++ site-form-works/car_admin/app/forms.py
@@ -1,3 +1,4 @@
+from django.utils.translation import gettext_lazy as _
 from django import forms
 from .models import Review
 
--- site-form-works/car_admin/app/models.py
+++ site-form-works/car_admin/app/models.py
@@ -2,21 +2,29 @@ from django.db import models
 
 
 class Car(models.Model):
-    brand = models.CharField(max_length=50)
-    model = models.CharField(max_length=50)
+    brand = models.CharField(max_length=50, verbose_name='марка')
+    model = models.CharField(max_length=50, verbose_name='модель')
 
     def __str__(self):
         return f'{self.brand} {self.model}'
 
     def review_count(self):
         return Review.objects.filter(car=self).count()
+    review_count.short_description = 'Количество обзоров'
+
+    class Meta:
+        verbose_name = 'машина'
+        verbose_name_plural = 'машины'
 
 
 class Review(models.Model):
-    car = models.ForeignKey(Car, on_delete=models.CASCADE)
+    car = models.ForeignKey(Car, verbose_name='машина', on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     text = models.TextField()
 
     def __str__(self):
         return str(self.car) + ' ' + self.title
 
+    class Meta:
+        verbose_name = 'обзор'
+        verbose_name_plural = 'обзоры'
